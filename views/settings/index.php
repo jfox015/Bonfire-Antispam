b5c06f5459ecc3a8b3f4315ba4e9a18d5aa7cd2a
Jeff Fox <jfox015@gmail.com>
2012-03-13 00:53:41 -0400

    Beginning 0.6-dev compatitility updates

--- views/settings/index.php
+++ views/settings/index.php
@@ -4,101 +4,122 @@
 </div>
 <?php endif; ?>
 
-<p class="small"><?php echo lang('bf_required_note'); ?></p>
 
-<?php echo form_open($this->uri->uri_string(), 'class="constrained ajax-form"'); ?>
+<div class="admin-box">
 
-		<!-- Eneable/Disable antispam -->
-	<div>
-		<label><?php echo lang('us_antispam_spambot'); ?></label>
-		<?php
-		$use_selection = ((isset($settings['antispam.antispam_enabled']) && $settings['antispam.antispam_enabled'] == 1) || !isset($settings['antispam.antispam_enabled'])) ? true : false;
-		echo form_checkbox('antispam_enabled',1, $use_selection,'id="antispam_enabled"');
-		?>
-	</div>
-    		<!-- OOTP DETAILS OVERRIDE 
-	<div>
-		<label for="antispam_type"><?php //echo lang('us_antispam_service'); ?></label>
-		<?php
-		//$use_selection = ((isset($antispam_type) && $antispam_type == 1) || !isset($antispam_type)) ? true : false;
-		//echo form_checkbox('antispam_type',1, $use_selection,'id="antispam_type"');
-		?>
-	</div>-->
+    <h3>General Settings</h3>
+    <?php echo form_open($this->uri->uri_string(), 'class="form-horizontal"'); ?>
+    <fieldset>
+
+        <!-- Eneable/Disable antispam -->
+        <div class="control-group <?php echo form_error('antispam_enabled') ? 'error' : '' ?>">
+            <label><?php echo lang('us_antispam_spambot') ?></label>
+            <div class="controls">
+                <?php
+                $use_selection = ((isset($settings['antispam.antispam_enabled']) && $settings['antispam.antispam_enabled'] == 1) || !isset($settings['antispam.antispam_enabled'])) ? true : false;
+                echo form_checkbox('antispam_enabled',1, $use_selection,'id="antispam_enabled"');
+                ?>
+                <?php if (form_error('antispam_enabled')) echo '<span class="help-inline">'. form_error('antispam_enabled') .'</span>'; ?>
+            </div>
+        </div>
+
     <div id="ootp_block">
-		<div>&nbsp;</div>
-        <div>
-		<?php echo lang('us_recapcha_note'); ?>
-		</div>
+
+        <legend>reCAPTCHA</legend>
+
+		<?php echo lang('us_recapcha_note'); ?><p /><br />
+
             <!-- reCAPTCHA AUTH -->
 		<fieldset>
-		<legend><?php echo lang('us_recapcha_auth'); ?></legend>
-		</fieldset>
-		<div>
-		<?php echo lang('us_recapcha_auth_note'); ?>
-		</div>
-		<div>
-            <label for="recaptcha_key_public"><?php echo lang('us_recapcha_public'); ?></label>
-            <input type="text" id="recaptcha_key_public" name="recaptcha_key_public" value="<?php echo (isset($settings['antispam.recaptcha_key_public'])) ? $settings['antispam.recaptcha_key_public']: set_value('antispam.recaptcha_key_public'); ?>" />
-        </div>
-            <!-- LEAGUE ABBR -->
-        <div>
-            <label for="recaptcha_key_private"><?php echo lang('us_recapcha_private'); ?></label>
-            <input type="text" class="small" id="recaptcha_key_private" name="recaptcha_key_private" value="<?php echo (isset($settings['antispam.recaptcha_key_private'])) ? $settings['antispam.recaptcha_key_private']: set_value('antispam.recaptcha_key_private'); ?>" />
-        </div>
-        
+            <legend><?php echo lang('us_recapcha_auth'); ?></legend>
+            <div>
+            <?php echo lang('us_recapcha_auth_note'); ?>
+            </div>
+
+            <div class="control-group <?php echo form_error('recaptcha_key_public') ? 'error' : '' ?>">
+                <label><?php echo lang('us_recapcha_public') ?></label>
+                <div class="controls">
+                    <input type="text" id="recaptcha_key_public" name="recaptcha_key_public" value="<?php echo (isset($settings['antispam.recaptcha_key_public'])) ? $settings['antispam.recaptcha_key_public']: set_value('antispam.recaptcha_key_public'); ?>" />
+                    <?php if (form_error('recaptcha_key_public')) echo '<span class="help-inline">'. form_error('recaptcha_key_public') .'</span>'; ?>
+                </div>
+            </div>
+
+            <div class="control-group <?php echo form_error('recaptcha_key_private') ? 'error' : '' ?>">
+                <label><?php echo lang('us_recapcha_private') ?></label>
+                <div class="controls">
+                    <input type="text" class="small" id="recaptcha_key_private" name="recaptcha_key_private" value="<?php echo (isset($settings['antispam.recaptcha_key_private'])) ? $settings['antispam.recaptcha_key_private']: set_value('antispam.recaptcha_key_private'); ?>" />
+                    <?php if (form_error('recaptcha_key_private')) echo '<span class="help-inline">'. form_error('recaptcha_key_private') .'</span>'; ?>
+                </div>
+            </div>
+        </fieldset>
+
 		<fieldset>
-		<legend><?php echo lang('us_recapcha_opt'); ?></legend>
-		</fieldset>
-		<div>
-			<label for="recaptcha_theme"><?php echo lang('us_recapcha_theme'); ?></label>
-			<select id="recaptcha_theme" name="recaptcha_theme" class="small" >
-			<?php
-				$themes = loadRecaptchaThemes();
-				foreach($themes as $id => $theme){
-					echo('<option value="'.$id.'"');
-					if (isset($settings['antispam.recaptcha_theme']) && $settings['antispam.recaptcha_theme'] == $id) {
-						echo(' selected="selected"');
-					}
-					echo('">'.$theme.'</option>');
-				} 
-			?>
-			</select>
-		</div>
-		<div>
-			<label for="recaptcha_lang"><?php echo lang('us_recapcha_lang'); ?></label>
-			<select id="recaptcha_lang" name="recaptcha_lang" class="small" >
-			<?php
-				$langs = loadRecaptchaLangs();
-				foreach($langs as $id => $lang){
-					echo('<option value="'.$id.'"');
-					if (isset($settings['antispam.recaptcha_lang']) && $settings['antispam.recaptcha_lang'] == $id) {
-						echo(' selected="selected"');
-					}
-					echo('">'.$lang.'</option>');
-				} 
-			?>
-			</select>
-		</div>
-		
-			<!-- AUTO LOAD -->
-		<div>
-			<label><?php echo lang('us_recapcha_stnd'); ?></label>
-			<?php
-			$compliant = (isset($settings['antispam.recaptcha_compliant']) && $settings['antispam.recaptcha_compliant'] == 1) ? true : false;
-			echo form_radio('recaptcha_compliant',1, $compliant);
-			echo(lang('us_recapcha_xhtml')."<br />");
-			?>
-			<label>&nbsp;</label>
-			<?php
-            $compliant = (isset($settings['recaptcha_compliant']) && $settings['recaptcha_compliant'] == -1) ? true : false;
-            echo form_radio('recaptcha_compliant',-1, $compliant);
-			echo(lang('us_recapcha_none'));
-			?>
-		</div>
-	
-	<div class="submits">
-		<input type="submit" name="submit" value="<?php echo lang('bf_action_save') ?> " /> <?php echo lang('bf_or') ?> <?php echo anchor(SITE_AREA .'/settings', lang('bf_action_cancel')); ?>
-	</div>
+		    <legend><?php echo lang('us_recapcha_opt'); ?></legend>
+                <!-- THRME -->
+            <div class="control-group <?php echo form_error('recaptcha_theme') ? 'error' : '' ?>">
+                <label><?php echo lang('us_recapcha_theme') ?></label>
+                <div class="controls">
+                    <select id="recaptcha_theme" name="recaptcha_theme" class="small" >
+                        <?php
+                        $themes = loadRecaptchaThemes();
+                        foreach($themes as $id => $theme){
+                            echo('<option value="'.$id.'"');
+                            if (isset($settings['antispam.recaptcha_theme']) && $settings['antispam.recaptcha_theme'] == $id) {
+                                echo(' selected="selected"');
+                            }
+                            echo('">'.$theme.'</option>');
+                        }
+                        ?>
+                    </select><?php if (form_error('recaptcha_theme')) echo '<span class="help-inline">'. form_error('recaptcha_theme') .'</span>'; ?>
+                </div>
+            </div>
+
+                <!-- Language -->
+            <div class="control-group <?php echo form_error('recaptcha_lang') ? 'error' : '' ?>">
+                <label><?php echo lang('us_recapcha_lang') ?></label>
+                <div class="controls">
+                    <select id="recaptcha_lang" name="recaptcha_lang" class="small" >
+                        <?php
+                        $langs = loadRecaptchaLangs();
+                        foreach($langs as $id => $lang){
+                            echo('<option value="'.$id.'"');
+                            if (isset($settings['antispam.recaptcha_lang']) && $settings['antispam.recaptcha_lang'] == $id) {
+                                echo(' selected="selected"');
+                            }
+                            echo('">'.$lang.'</option>');
+                        }
+                        ?>
+                    </select>
+                    <?php if (form_error('recaptcha_lang')) echo '<span class="help-inline">'. form_error('recaptcha_lang') .'</span>'; ?>
+                </div>
+            </div>
+
+                <!-- AUTO LOAD -->
+            <div class="control-group <?php echo form_error('recaptcha_compliant') ? 'error' : '' ?>">
+                <label><?php echo lang('us_recapcha_stnd') ?></label>
+                <div class="controls">
+                    <?php
+                    $compliant = (isset($settings['antispam.recaptcha_compliant']) && $settings['antispam.recaptcha_compliant'] == 1) ? true : false;
+                    echo form_radio('recaptcha_compliant',1, $compliant);
+                    echo(lang('us_recapcha_xhtml')."<br />");
+                    ?>
+                    <label>&nbsp;</label>
+                    <?php
+                    $compliant = (isset($settings['recaptcha_compliant']) && $settings['recaptcha_compliant'] == -1) ? true : false;
+                    echo form_radio('recaptcha_compliant',-1, $compliant);
+                    echo(lang('us_recapcha_none'));
+                    ?>
+                    <?php if (form_error('recaptcha_compliant')) echo '<span class="help-inline">'. form_error('recaptcha_compliant') .'</span>'; ?>
+                </div>
+            </div>
+    </div>
+    </fieldset>
+
+    <div class="form-actions">
+        <input type="submit" name="submit" class="btn primary" value="<?php echo lang('bf_action_save') ?> " /> <?php echo lang('bf_or') ?> <?php echo anchor(SITE_AREA .'/settings', lang('bf_action_cancel')); ?>
+    </div>
+
+</div>
 	
 <?php echo form_close(); ?>
 
